b6213879b953566f48164810513fed781da997a8
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-12-10 17:34:56 +0000

    Create a Model

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 
-let dotenv =require('dotenv').config();
+let dotenv = require('dotenv').config();
 let mongoose = require("mongoose")
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -11,7 +11,7 @@ const personSchema = new mongoose.Schema({
   age: {type :Number},
   favoriteFoods: [String],
 });
- Person = model.Person || model("Person",personSchema)
+ Person = mongoose.model.Person || mongoose.model("Person",personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
