e0c237bc604c3b20f5c953a47bc30c040b87031e
melvin-phyllis <melvinphyllisakou@gmail.com>
2025-12-10 17:37:12 +0000

    Create a Model

--- myApp.js
+++ myApp.js
@@ -7,11 +7,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 let Person;
 
 const personSchema = new mongoose.Schema({
-  name: {tupe :String},
-  age: {type :Number},
-  favoriteFoods: [String],
+  name: { tupe: String },
+  age: { type: Number },
+  favoriteFoods: { type: [String] }
 });
- Person = mongoose.model.Person || mongoose.model("Person",personSchema)
+Person = mongoose.model.Person || mongoose.model("Person", personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
